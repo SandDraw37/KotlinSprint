3c66b5cffb4ffb4177acd922a53f31b977c01dd4
SandDrawIT286 <sony37@mail.ru>
2023-04-02 13:26:56 +0300

    Initial Commit

---
+++ src/main/kotlin/KS-1-1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val sumOfWebSiteOrders: Int = 75 /* количество заказов*/
+    var gratitudeForCustomer: String = "Спасибо, что воспользовались нашим магазином!" /* текст с благодарностью за покупку */
+
+    println(sumOfWebSiteOrders)
+    println(gratitudeForCustomer)
+}
---
+++ src/main/kotlin/KS-1-3.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val year: Int = 1961 /* год полета Юрия Гагарина в космос */
+    var hour: String = "09" /* час взлета */
+    var minute: String = "07" /* минута взлета */
+
+    println(year)
+    //println(hour)
+    //println(minute)
+
+    hour = "10" /* час посадки */
+    minute = "55" /* минута посадки */
+
+    println(hour+":"+minute)
+}
+
